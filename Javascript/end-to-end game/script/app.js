0449bc4503290146618214689f2657be56a34e35
Kjintae <wlsxo2112@naver.com>
2019-07-18 17:19:54 +0900

    commit

--- Javascript/end-to-end game/script/app.js
+++ Javascript/end-to-end game/script/app.js
@@ -4,11 +4,11 @@ var word = '끝말잇기';
 var answer = prompt(word);
     while(true) { 
         if(word[word.length - 1] === answer[0]) { // '끝말잇기' 길이의 요소에서 -1한것과 다음 글의 0번 째 요소를 비교.
-            //맞았을 때
+            //맞았을 때 
             alert('제대로 하고있군요. 다음 글을 입력해주세요.');
             word = answer;  
         } else {
-            //틀렸을 때
+            //틀렸을 때 
             alert('다시 입력해주세요.');
         }
     }
